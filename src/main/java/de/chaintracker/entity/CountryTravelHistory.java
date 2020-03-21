e27d9e1b57052ccb7aadcb538d2ba733a8f94645
Marko Voss <markovoss@arcor.de>
2020-03-21 01:59:17 +0100

    Added CountryTravelHistory entity.

---
+++ src/main/java/de/chaintracker/entity/CountryTravelHistory.java
@@ -0,0 +1,65 @@
+/**
+ *
+ */
+package de.chaintracker.entity;
+
+import java.time.OffsetDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import org.hibernate.annotations.GenericGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Marko Voß
+ *
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CountryTravelHistory {
+
+  @Id
+  @GeneratedValue(generator = "uuid")
+  @GenericGenerator(name = "uuid", strategy = "uuid")
+  @Column(updatable = false, nullable = false, unique = true, length = 32)
+  private String id;
+
+  @Column(name = "timestamp_create", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+  private OffsetDateTime timestampCreate;
+
+  @Column(name = "timestamp_update", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+  private OffsetDateTime timestampUpdate;
+
+  private boolean isDeleted;
+  private boolean isEnabled;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  private User userCreate;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  private User userUpdate;
+
+  @Column(length = 32)
+  private String cityName;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  private Country country;
+
+  @Column(name = "date_travel_from", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+  private OffsetDateTime timestampTravelFrom;
+
+  @Column(name = "date_travel_to", columnDefinition = "TIMESTAMP WITH TIME ZONE")
+  private OffsetDateTime timestampTravelTo;
+
+  @Column(length = 250)
+  private String note;
+}
