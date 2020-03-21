250742001800851a479c8e02d9e61c2f06f9dbf1
Marko Voss <markovoss@arcor.de>
2020-03-21 17:40:32 +0100

    Added LocationEvent endpoint.

--- src/main/java/de/chaintracker/dto/LocationEventDto.java
+++ src/main/java/de/chaintracker/dto/LocationEventDto.java
@@ -1,5 +1,27 @@
 package de.chaintracker.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class LocationEventDto {
+
+  @Size(max = 32, min = 32)
+  private String id;
+
+  @NotNull
+  private Double latitude;
+
+  @NotNull
+  private Double longitude;
+
+  @Size(max = 120)
+  private String externalId;
+
+  @Size(max = 250)
+  private String name;
 
 }
