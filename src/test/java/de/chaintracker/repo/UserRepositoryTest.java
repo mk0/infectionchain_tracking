ca9753b4d84a2a9607dd08439e154582a23be986
Marko Voss <markovoss@arcor.de>
2020-03-21 02:45:34 +0100

    Added UserRepositoryTest.

---
+++ src/test/java/de/chaintracker/repo/UserRepositoryTest.java
@@ -0,0 +1,63 @@
+/**
+ *
+ */
+package de.chaintracker.repo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.nio.charset.StandardCharsets;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.google.common.hash.Hashing;
+import de.chaintracker.entity.User;
+import de.chaintracker.repo.UserRepository;
+
+/**
+ * @author Marko Voß
+ *
+ */
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+  @Autowired
+  private DataSource dataSource;
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+  @Autowired
+  private EntityManager entityManager;
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  void injectedComponentsAreNotNull() {
+    assertThat(this.dataSource).isNotNull();
+    assertThat(this.jdbcTemplate).isNotNull();
+    assertThat(this.entityManager).isNotNull();
+    assertThat(this.userRepository).isNotNull();
+  }
+
+  @Test
+  void test_userCreation() {
+
+    final User createdUser = this.userRepository.save(User.builder()
+        .email("max.mustermann@example.com")
+        .encryptedPassword(Hashing.sha256().hashString("Hallo123!", StandardCharsets.UTF_8).toString())
+        .firstName("Max")
+        .lastName("Mustermann")
+        .userId("@Max")
+        .userName("max.mustermann")
+        .build());
+
+    assertThat(this.userRepository.findById(createdUser.getId())).isNotNull();
+    assertThat(createdUser.getEmailVerificationToken()).isNotNull();
+    assertThat(createdUser.getTimestampCreate()).isNotNull();
+    assertThat(createdUser.getTimestampUpdate()).isNull();
+  }
+}
+
