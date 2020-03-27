89f7e92bf616f2803dceefb1b63f3ad68c112d7b
Marko Voss <markovoss@arcor.de>
2020-03-27 13:05:47 +0100

    Added logging of requests for better error analysis.

---
+++ src/main/java/de/chaintracker/config/LoggingFilterConfig.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package de.chaintracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * @author Marko Voß
+ *
+ */
+@Configuration
+public class LoggingFilterConfig {
+
+  @Bean
+  public CommonsRequestLoggingFilter logFilter() {
+    final CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeQueryString(true);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(10000);
+    filter.setIncludeHeaders(false);
+    return filter;
+  }
+}
